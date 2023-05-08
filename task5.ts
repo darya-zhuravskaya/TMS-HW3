facc4137643f0a87aebe3ac5c918cccd1662bc39
darya-zhuravskaya <darya.zhuravskaya159@gmail.com>
2023-05-09 00:03:15 +0200

    finished task 5

---
+++ task5.ts
@@ -0,0 +1,49 @@
+let str1 = 'доступ';
+let str2 = 'морпех';
+let str3 = 'наледь';
+let str4 = 'попрек';
+let str5 = 'рубило';
+let lengthWords = 0;
+
+let index = 0;
+while(str1[index]){
+    console.log(str1[index])
+    index = index + 1
+}
+// index = 6
+
+lengthWords = lengthWords + index;
+
+
+index = 0;
+while(str2[index]){
+    console.log(str2[index])
+    index = index + 1
+}
+
+lengthWords = lengthWords + index;
+
+index = 0;
+while(str3[index]){
+    console.log(str3[index])
+    index = index + 1
+}
+
+lengthWords = lengthWords + index;
+
+index = 0;
+while(str4[index]){
+    console.log(str4[index])
+    index = index + 1
+}
+
+lengthWords = lengthWords + index;
+
+index = 0;
+while(str5[index]){
+    console.log(str5[index])
+    index = index + 1
+}
+
+lengthWords = lengthWords + index;
+console.log(lengthWords)
