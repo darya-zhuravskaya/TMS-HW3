9575760efbb04668abd1286cee0df66a231f4ba8
darya-zhuravskaya <darya.zhuravskaya159@gmail.com>
2023-05-08 23:12:12 +0200

    Improved task4

--- task4.ts
+++ task4.ts
@@ -1,8 +1,6 @@
 let a = 1;
 let b = 2;
 let c = 'белых медведей';
-let space = ' ';
-let aString = String(a);
-let bString = String(b);
-let result = aString + bString + space + c;
+
+let result = `${a}${b} ${c}`;
 console.log(result)
