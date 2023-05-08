19a17a2f2e3d2ed81c055f4b78f40b0d0577df4c
darya-zhuravskaya <darya.zhuravskaya159@gmail.com>
2023-05-08 22:59:23 +0200

    Finished 4 tasks

---
+++ task4.ts
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 2;
+let c = 'белых медведей';
+let space = ' ';
+let aString = String(a);
+let bString = String(b);
+let result = aString + bString + space + c;
+console.log(result)
