5c238c476bca24a39639754ee37bd841692844a1
darya-zhuravskaya <darya.zhuravskaya159@gmail.com>
2023-05-10 01:16:19 +0200

    Finished task 13

---
+++ task11.ts
@@ -0,0 +1,11 @@
+let l = 15
+if (l % 5 === 0 && l % 3 === 0)
+{
+    console.log("FizBuz");
+} else if (l % 3 === 0)
+{
+    console.log("Buz");
+} 
+else if(l % 5 === 0){
+    console.log("Fiz");
+}
